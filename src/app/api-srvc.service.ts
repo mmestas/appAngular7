aa6ca038e2bbdf9831bffd583a01319374a0499f
mmestas <monette@contensive.com>
2019-07-01 06:54:40 -0400

    url changes

--- src/app/api-srvc.service.ts
+++ src/app/api-srvc.service.ts
@@ -34,15 +34,14 @@ const httpPostOptions =
 })
 export class ApiSrvc {
 
-  dashboardListUrl = 'http://addonlist.monette.sitefpo.com/admin?remotemethodaddon=DemoDashboardArray';
-  demoListUrl = 'http://addonlist.monette.sitefpo.com/admin?remotemethodaddon=DemoRemoteMethod';
-  complexObjUrl = 'http://addonlist.monette.sitefpo.com/admin?remotemethodaddon=DemoComplexObject';
-  dragDropUrl = 'http://addonlist.monette.sitefpo.com/admin?remotemethodaddon=DemoDragDrop';
-  memberbossDragDropUrl = 'http://memberboss.com/getaddonlist';
-  memberbossPanelListUrl = 'http://memberboss.com/getaddonPanellist';
-  memberbossAddonListUrl = 'http://memberboss.com/getAddonList';
-  memberbossSetAddonListUrl = 'http://memberboss.com/setAddonList'; //Keep Correct
-  // memberbossSetAddonListUrl = 'http://memberboss.com/setsampleaddonlist'; //Only for testing
+  dashboardListUrl = 'http://url/admin?remotemethodaddon=DemoDashboardArray';
+  demoListUrl = 'http://aurl/admin?remotemethodaddon=DemoRemoteMethod';
+  complexObjUrl = 'http://aurl/admin?remotemethodaddon=DemoComplexObject';
+  dragDropUrl = 'http://aurl/admin?remotemethodaddon=DemoDragDrop';
+  memberbossDragDropUrl = 'http://url/getaddonlist';
+  memberbossPanelListUrl = 'http://url/getaddonPanellist';
+  memberbossAddonListUrl = 'http://url/getAddonList';
+  memberbossSetAddonListUrl = 'http://url/setAddonList';
 
   constructor(
     private http: HttpClient,
@@ -107,14 +106,14 @@ export class ApiSrvc {
 
     return this.http.post(this.memberbossSetAddonListUrl, {data}, httpGetOptions)
 
-    // return this.http.post('http://memberboss.com/setsampleaddonlist?parentContentGuid='+data.parentContentGuid+'&parentRecordGuid='+data.parentRecordGuid)
-      // return this.http.post('http://memberboss.com/setAddonlist?parentContentGuid='+data.parentContentGuid+'&parentRecordGuid='+data.parentRecordGuid)
+    // return this.http.post('http://url/setsampleaddonlist?parentContentGuid='+data.parentContentGuid+'&parentRecordGuid='+data.parentRecordGuid)
+      // return this.http.post('http://url/setAddonlist?parentContentGuid='+data.parentContentGuid+'&parentRecordGuid='+data.parentRecordGuid)
       //
       // .pipe();
   }
 
   // getDragNDrop() {
-  //   return this.http.post('http://memberboss.com/getaddonlist', {
+  //   return this.http.post('http://url/getaddonlist', {
   //     "parentContentGuid":"{02203F58-0921-4F75-A089-0EAD6E8FEB4F}",
   //     "parentRecordGuid":"{925F4A57-32F7-44D9-9027-A91EF966FB0D}"
   //   }, httpGetOptions)
